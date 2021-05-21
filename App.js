1f5304087e43548ab8e70f809db63190322abe3f
SJ_Win <jeen0202@korea.ac.kr>
2021-05-21 15:46:51 +0900

    프로젝트 재설정

--- App.js
+++ App.js
@@ -1,26 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {StackNavigator} from 'react-navigation'
-import MainScreen from './components/MainScreen'
+import 'react-native-gesture-handler'
+import {NavigationContainer } from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import LandingScreen from './components/Landing'
 
+const Stack = createStackNavigator(); 
 export default function App() {
-  return (    
-      <AppStackNavigator/> 
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreen} 
+        options={{headerShow : false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const AppStackNavigator = StackNavigator({
-  Main:{
-    screen: MainScreen
-  }
-})
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
