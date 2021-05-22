5b8f5e9fa78c250f19100d642f0309b8739aecc8
sejing <jeen0202@korea.ac.kr>
2021-05-22 20:50:51 +0900

    App에서 fiebase 초기화

--- App.js
+++ App.js
@@ -3,8 +3,24 @@ import React from 'react';
 import 'react-native-gesture-handler'
 import {NavigationContainer } from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
-import LandingScreen from './components/Landing'
+import LandingScreen from './components/auth/Landing'
 
+//firebase 사용
+import firebase from 'firebase/app'; //기존 firebase => firbase/app
+//firbase console에서 config 복붙
+const firebaseConfig = {
+  apiKey: "AIzaSyDdJmkm74EDyksSyfKuGdvCkhebsLEmm90",
+  authDomain: "insta-clone-f4cda.firebaseapp.com",
+  projectId: "insta-clone-f4cda",
+  storageBucket: "insta-clone-f4cda.appspot.com",
+  messagingSenderId: "650587959480",
+  appId: "1:650587959480:web:5b1e88d74d5865d507444d",
+  measurementId: "G-MF4KN4LSC4"
+};
+//firebase 초기화
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig)
+}
 const Stack = createStackNavigator(); 
 export default function App() {
   return (
--- README.md
+++ README.md
@@ -10,3 +10,8 @@ npm install @react-natvigation/stack
 1. 새 프로젝트 생성
 2. Authentication 설정
     sign-in-method에서 이메일/비밀번호 사용설정
+3. Web app 등록
+    Setting-구성(Config)로 SDK snippet 사용
+4. Expo에서 Firebase 사용 설정
+    https://docs.expo.io/guides/using-firebase/ 참고
+    expo install firebase
