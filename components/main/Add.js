b9cbc03de70143063ae4505855cfb5ed1e4168b2
SJ_Win <jeen0202@korea.ac.kr>
2021-05-24 15:22:42 +0900

    카메라와 갤러리 사용권한 허용정보 반영

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, Image} from 'react-native';
 import { Camera } from 'expo-camera';
-
+//갤러리에서 사진을 불러오기 위한 package
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
     //카메라 사용 권한 확인
-    const [hasPermission, setHasPermission] = useState(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
     //카메라 접근
     const [camera,setCamera] = useState(null);
@@ -13,8 +15,12 @@ export default function App() {
 
     useEffect(() => {
     (async () => {
-        const { status } = await Camera.requestPermissionsAsync();
-        setHasPermission(status === 'granted');
+        const cameraStatus = await Camera.requestPermissionsAsync();
+        setHasCameraPermission(cameraStatus.status === 'granted');
+
+        const galleyStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        setHasGalleryPermission(galleyStatus.status === 'granted');
+
     })();
     }, []);
 
@@ -26,10 +32,10 @@ export default function App() {
         }
     }
 
-    if (hasPermission === null) {
+    if (hasCameraPermission === null||hasGalleryPermission === null) {
     return <View />;
     }
-    if (hasPermission === false) {
+    if (hasCameraPermission === false || hasGalleryPermission === false) {
     return <Text>No access to camera</Text>;
     }
   return (
