ceea8acd145dc0f87c931e288855e82df15250f7
SJ_Win <jeen0202@korea.ac.kr>
2021-05-24 17:00:28 +0900

    firebase-storage에 저장 적용중

--- App.js
+++ App.js
@@ -22,13 +22,13 @@ import thunk from 'redux-thunk'
 const store = createStore(rootReducer, applyMiddleware(thunk))
 //firbase console에서 config 복붙
 const firebaseConfig = {
-  apiKey: "AIzaSyDdJmkm74EDyksSyfKuGdvCkhebsLEmm90",
-  authDomain: "insta-clone-f4cda.firebaseapp.com",
-  projectId: "insta-clone-f4cda",
-  storageBucket: "insta-clone-f4cda.appspot.com",
-  messagingSenderId: "650587959480",
-  appId: "1:650587959480:web:5b1e88d74d5865d507444d",
-  measurementId: "G-MF4KN4LSC4"
+    apiKey: "AIzaSyDdJmkm74EDyksSyfKuGdvCkhebsLEmm90",
+    authDomain: "insta-clone-f4cda.firebaseapp.com",
+    projectId: "insta-clone-f4cda",
+    storageBucket: "insta-clone-f4cda.appspot.com",
+    messagingSenderId: "650587959480",
+    appId: "1:650587959480:web:5b1e88d74d5865d507444d",
+    measurementId: "G-MF4KN4LSC4"
 };
 //firebase 초기화
 if(firebase.apps.length === 0){
--- components/main/Save.js
+++ components/main/Save.js
@@ -1,11 +1,46 @@
-import React from 'react'
-import {View, TextInput, Imgae} from 'react-native'
+import React,{useState} from 'react'
+import {View, TextInput, Image,Button} from 'react-native'
+
+import firebase from 'firebase/app'
+require("firebase/firestore")
+require("firebase/firebase-storage")
 
 export default function Save(props) {
-    console.log(props)
-    return (
-        <View>
+    // console.log(props.route.params.image)
+    //text 입력을 위한 hook 사용
+    const [caption, setCaption] = useState("")
+    
+    const uploadImage= async () =>{
+        const uri = props.route.params.image;
+        const childPath = `post/${firebase.auth().currentUser.uid}/${Math.random.toString(36)}`
+        console.log(childPath)
 
+        const response = await fetch(uri)
+        const blob = await response.blob();
+        //firebase-storage에 random한 id로 data 저장
+        const task = firebase.storage().ref().child(childPath).put(blob);
+
+        const taskProgress = snapshot => {
+            console.log(`transferred: ${snapshot.bytesTransferred}`)
+        }
+        const taskCompleted = () => {
+            task.snapshot.ref.getDownloadURL().then((snapshot) => {
+                console.log(snapshot)
+            })
+        }
+        const taskError = snapshot => {
+            console.log(snapshot)
+        }
+        task.on("state_changed", taskProgress,taskError,taskCompleted);
+    }
+    return (
+        <View style={{flex:1}}>
+            <Image source={{uri:props.route.params.image}}/>
+            <TextInput
+                placeholder="Write a Caption . . ."
+                onChangeText={(caption) => setCaption(caption)}
+            />
+            <Button title="Save" onPress={()=> uploadImage()}/>
         </View>
     )
 }
