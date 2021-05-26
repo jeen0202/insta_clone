41c1417a47c32d4552d648b96017d4c52e0108ab
SJ_Win <jeen0202@korea.ac.kr>
2021-05-26 09:39:03 +0900

    Redux를 사용하여 firestore에서 Follow한 사용자의 post 추출

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-import {USER_POST_STATE_CHANGE, USER_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE} from '../constants/index'
+import {USER_POST_STATE_CHANGE, USER_STATE_CHANGE, USERS_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE, USERS_DATA_STATE_CHANGE} from '../constants/index'
 export function fetchUser(){
     return ((dispatch) => {
         firebase.firestore()
@@ -46,7 +46,60 @@ export function fetchUserFollowing(){
                     const id = doc.id;
                     return id
                 })                
-                dispatch({type: USER_FOLLOWING_STATE_CHANGE, following})
+                dispatch({type: USER_FOLLOWING_STATE_CHANGE, following});
+                for(let i = 0; i < following.length; i++){
+                    dispatch(fetchUsersData(following[i]));
+                }
             })
     })
 }
+
+export function fetchUsersData(uid) {
+    return ((dispatch, getState) => {
+        const found = getState().usersState.users.some(el => el.uid === uid)
+
+        if(!found){
+            firebase.firestore()
+            .collection("users")
+            .doc(uid)
+            .get()
+            .then((snapshot) => {
+                if(snapshot.exists){
+                    let user = snapshot.data();
+                    user.uid = snapshot.id;                    
+                    dispatch({type : USERS_DATA_STATE_CHANGE, user});
+                    dispatch(fetchUsersFollowingPosts(user.uid));
+                }else{
+                    console.log('does not exist')
+                }
+            })            
+        }
+    })
+}
+//synchronous임에 유의
+export function fetchUsersFollowingPosts(uid){
+    return ((dispatch, getState) => {
+        firebase.firestore()
+            .collection("posts")
+            .doc(uid)
+            .collection("userPosts")
+            .orderBy("creation", "asc")
+            .get()
+            .then((snapshot) => {
+
+                //const uid = snapshot.query.EP.path.segments[1];
+                const uid = snapshot.docs[0].ref.path.split('/')[1]                
+                console.log({snapshot, uid});
+                const user = getState().usersState.users.find(el => el.uid === uid)
+                let posts = snapshot.docs.map(doc => {
+                    const data = doc.data();
+                    const id = doc.id;
+                    return{id, ...data, user}                    
+                })            
+                console.log(posts)    
+                dispatch({type: USERS_POSTS_STATE_CHANGE, posts,uid})
+                console.log(getState())
+            })
+    })
+}
+
