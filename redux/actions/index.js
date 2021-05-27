cc17e6074f48bdd74192082e708d644b07432c03
SJ_Win <jeen0202@korea.ac.kr>
2021-05-27 15:47:41 +0900

    Like/Dislike 기능 구현을 위한 redux 설정

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-import {CLEAR_DATA,USER_POST_STATE_CHANGE, USER_STATE_CHANGE, USERS_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE, USERS_DATA_STATE_CHANGE} from '../constants/index'
+import {CLEAR_DATA,USER_POST_STATE_CHANGE, USER_STATE_CHANGE, USERS_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE, USERS_DATA_STATE_CHANGE, USERS_LIKES_STATE_CHANGE} from '../constants/index'
 require('firebase/firestore')
 
 export function clearData() {
@@ -106,10 +106,34 @@ export function fetchUsersFollowingPosts(uid){
                     const id = doc.id;
                     return{id, ...data, user}                    
                 })            
-               // console.log(posts)    
+               // console.log(posts)
+               for(let i =0; i< posts.length;i++){
+                   dispatch(fetchUsersFollowingLikes(uid, posts[i].id))
+               }    
                 dispatch({type: USERS_POSTS_STATE_CHANGE, posts,uid})
                // console.log(getState())
             })
     })
 }
 
+export function fetchUsersFollowingLikes(uid, postId){
+    return ((dispatch) => {
+        firebase.firestore()
+            .collection("posts")
+            .doc(uid)
+            .collection("userPosts")
+            .doc(postId)
+            .collection("likes")    
+            .doc(firebase.auth().currentUser.uid)
+            .onSnapshot((snapshot) => {                
+                const postId = snapshot.ref.path.split('/')[3]
+                //const postId = snapshot.ZE.path.segments[3];
+                let currentUserLike = false;
+                if(snapshot.exists){
+                    currentUserLike = true;
+                }
+                dispatch({type: USERS_LIKES_STATE_CHANGE, postId, currentUserLike});
+              
+            })
+    })
+}
--- redux/reducers/users.js
+++ redux/reducers/users.js
@@ -1,8 +1,9 @@
-import {CLEAR_DATA, USERS_DATA_STATE_CHANGE,USERS_POSTS_STATE_CHANGE  } from "../constants"
+import {CLEAR_DATA, USERS_DATA_STATE_CHANGE,USERS_POSTS_STATE_CHANGE,USERS_LIKES_STATE_CHANGE  } from "../constants"
 
 
 const initState = {
     users : [],
+    feed: [],
     usersFollowingLoaded : 0,
 }
 
@@ -17,10 +18,15 @@ export const users = (state = initState, action) => {
             return {
                 ...state,
                 usersFollowingLoaded : state.usersFollowingLoaded +1,
-                users : state.users.map(user => user.uid === action.uid ? 
-                    {...user,posts:action.posts} :
-                    user)
+                feed : [...state.feed, ...action.posts]
             } 
+        case USERS_LIKES_STATE_CHANGE:
+            return{
+                ...state,
+                feed : state.feed.map(post => post.id == action.postId ?
+                    {...post, currentUserLike: action.currentUserLike}:
+                    post)
+            }
             case CLEAR_DATA:
                 return initState         
         default:
