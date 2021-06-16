fb5e5a046ce190ca31097943414134a90792e081
sejing <jeen0202@korea.ac.kr>
2021-06-16 22:39:42 +0900

    State,Effect 사용

--- components/main/Story.js
+++ components/main/Story.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {connect} from 'react-redux'
 import {StyleSheet} from 'react-native'
 import { Container, Content, Text } from 'native-base'
 function Story(props) {
+    const [selectedIndex, setSelectedIndex] = useState(0)
 
+    useEffect(()=>{
+        setSelectedIndex(props.route.params.selectedIndex)
+    },[props.route.params.selectedIndex])
     return (
         <Container style={{flex:1}}>
             <Content contentContainerStyle={styles.content}>
                 <Text>Story Screen</Text>
-                <Text>Seleted Index is {props.route.params.selectedIndex}</Text>
+                <Text>Seleted Index is {selectedIndex}</Text>
             </Content>
         </Container>
     )
