c2332bf56e52e768d02370975937bb7906b899e4
SJ_Win <jeen0202@korea.ac.kr>
2021-06-17 15:33:45 +0900

    imgae one by one 출력 및 scroll 제한

--- components/main/Story.js
+++ components/main/Story.js
@@ -1,40 +1,64 @@
 import React,{useState,useEffect} from 'react'
 import {connect} from 'react-redux'
-import {StyleSheet} from 'react-native'
+import {StyleSheet,FlatList,Image,View,TouchableOpacity,Dimensions} from 'react-native'
 import moment from 'moment'
 import { Container, Content, Text } from 'native-base'
+
+const {width,height} = Dimensions.get('window');
 function Story(props) {
     const [selectedIndex, setSelectedIndex] = useState(0)
     const [images, setImages] = useState([])
-    useEffect(()=>{
+    useEffect(()=>{             
         props.feed.sort(function(x,y) {
             return y.creation - x.creation;
-        })
-        setSelectedIndex(props.route.params.selectedIndex)
+        })        
         //console.log(props.feed)
         //console.log(props.following)   
         //`${moment(item.creation.toDate()).format('YY년MM월DD일 HH:mm')}`     
         const makeImageArray= (feed,following) => {
+            let images = []
             for(let i=0;i<following.length;i++){
                 for(let j=0;j<feed.length;j++){
                     if(feed[j].user.uid === following[i]){
-                        console.log(feed[j].user.uid,feed[j].creation.toDate());                        
+                        //console.log(feed[j].user.uid,feed[j].creation.toDate());
+                        images[i]=feed[j].downloadURL;                                              
                         break;
-                    }
-                             
-                }
-                
+                    }                             
+                }                
             }
+            setImages(images)
         }
         makeImageArray(props.feed, props.following)
-        
-    },[props.route.params.selectedIndex])
+       
+        //console.log(images)
+    },[props.feed,props.following])
+    useEffect(()=>{
+        setSelectedIndex(parseInt(props.route.params.selectedIndex))
+        console.log(selectedIndex)   
+    },[selectedIndex,props.route.params.selectedIndex])
     return (
-        <Container style={{flex:1}}>
-            <Content contentContainerStyle={styles.content}>
-                <Text>Story Screen</Text>
-                <Text>Seleted Index is {selectedIndex}</Text>
-            </Content>
+        <Container style={{flex:1}}>            
+            <FlatList              
+            pagingEnabled={true}                    
+            showsHorizontalScrollIndicator={false}
+            //legacyImplementation={false}
+            disableIntervalMomentum={true}
+            numColumns={1}
+            initialNumToRender={2}
+            horizontal={false}
+            keyExtractor={(item,index)=> index.toString()}
+            data={images}            
+            renderItem={({item,index}) => (
+            <TouchableOpacity
+            style={{flex:1,justifyContent:'center'}}
+            onPress={()=>{}}> 
+              <View style={{flex:1,width,height}}>
+                <Image 
+                style={styles.image}                
+                source={{uri:item}} /> 
+              </View>
+            </TouchableOpacity>
+            )}/>            
         </Container>
     )
 }
@@ -44,7 +68,14 @@ const styles = StyleSheet.create({
         flex:1,
         alignItems:'center',
         justifyContent:'center'
-    }
+    },
+    image: {
+        flex:1,        
+        height:'100%',
+        width:'100%',
+        resizeMode:'contain',
+
+    },
 })
 const mapStatetoProps = (store) => ({
     currentUser : store.userState.currentUser,
