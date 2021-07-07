6ce8d33e0d090dbb7156ba28edfa8944db28fa78
SJ_Win <jeen0202@korea.ac.kr>
2021-07-07 11:59:53 +0900

    검색에서 대소문자 무시 기능 구현

--- components/main/Search.js
+++ components/main/Search.js
@@ -1,6 +1,6 @@
 import React, {useState}from 'react'
 import { Image, Text, TextInput, FlatList, TouchableOpacity,StyleSheet} from 'react-native'
-import {Container,Header,Left,Right,Icon,Button,Thumbnail} from 'native-base'
+import {Container,Header,Left,Right,Icon,Input,Button,Item,Thumbnail} from 'native-base'
 import insta_logo from '../../assets/insta_logo.png'
 import firebase from 'firebase'
 require('firebase/firestore')
@@ -8,11 +8,10 @@ export default function Search(props) {
     const [users, setUsers] = useState([])
 
     const fetchUsers = (search) => {
+        let result =[]
         const offset = search+"\uF7FF";
         firebase.firestore()
-        .collection('users')
-        .where('name','>=',search)
-        .where('name', '<=',offset )
+        .collection('users')        
         .get()
         .then((snapshot)=>{
             let users = snapshot.docs.map(doc => {
@@ -20,7 +19,10 @@ export default function Search(props) {
                 const id = doc.id;                
                 return{id, ...data}
             })
-            setUsers(users); 
+            result = users.filter ((el) => {
+                return (search.toString().toLowerCase() <=el.name.toString().toLowerCase()  && el.name.toString().toLowerCase() <= (search.toString().toLowerCase()+"\uF7FF"))          
+            });
+            setUsers(result); 
         })
     }
     return (
@@ -42,11 +44,22 @@ export default function Search(props) {
                 <Icon name='dots-vertical' type='MaterialCommunityIcons' style={{color:'black', fontSize:23}}/>
                 </Button>
             </Right>
-            </Header> 
-            <TextInput
+            </Header>
+            <Header searchBar rounded style={{backgroundColor:'white'}}>
+                <Item style={{backgroundColor:'#F3F3F3'}}>
+                    <Icon name="ios-search" />
+                    <Input placeholder="Search" 
+                    autoCapitalize="none"
+                    onChangeText={(search) => fetchUsers(search)}/>                    
+                </Item>
+                <Button transparent>
+                    <Text>Search</Text>
+                </Button>
+            </Header>
+            {/* <TextInput
                 style={{padding:10,fontSize:15}} 
                 placeholder="Type Here..."
-                onChangeText={(search)=>fetchUsers(search)}/>
+                onChangeText={(search)=>fetchUsers(search)}/> */}
 
             <FlatList
                 style={{marginLeft:10}}
